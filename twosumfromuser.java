aaa57defd0d599cb0489febce1b0b3743269d819
Prasant Kumar <prashantsingh8578@gmail.com>
2025-11-21 23:25:27 +0530

    Add files via upload

---
+++ twosumfromuser.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class twosumfromuser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();      // reads one word
+        System.out.println(input);
+
+        String name = sc.nextLine();   // reads full line
+        System.out.println(name);
+
+        int number = sc.nextInt();     // reads integer
+        System.out.println(number);
+
+        float decimalnumber = sc.nextFloat();  // reads float
+        System.out.println(decimalnumber);
+    }
+}
