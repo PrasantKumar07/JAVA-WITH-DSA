f4bc04aea30abb26bdd2c8a08489978840a7d53e
Prasant Kumar <prashantsingh8578@gmail.com>
2025-11-26 23:27:13 +0530

    Add files via upload

---
+++ binarysearch.java
@@ -0,0 +1,28 @@
+public class binarysearch {
+    public static int binarySearch(int numbers[], int key ){
+        int start = 0;
+        int end = numbers.length-1;
+
+        while(start <=end){
+            int mid = (start + end)/2;
+            
+            if(numbers[mid] == key){
+                return mid;
+            }
+            if (numbers[mid] < key){
+                start = mid +1;
+            }
+            if (numbers[mid] > key){
+                end = mid -1;
+            }
+        
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+       int numbers[]  = {2,4,6,8,10,12,14,16,18,20};
+       int key = -45;
+       System.out.println("Key found at index: " + binarySearch(numbers, key));
+
+    }
+}
