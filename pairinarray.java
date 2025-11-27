fb0d30777009767b7ee36b2ea4ae20ef19bd4379
Prasant Kumar <prashantsingh8578@gmail.com>
2025-11-27 23:36:47 +0530

    Add files via upload

---
+++ pairinarray.java
@@ -0,0 +1,30 @@
+//find all posibble pairs in an array
+/*
+2| 4 |6|8|10|
+
+(2,4) (2,6) (2,8) (2,10)
+(4,6) (4,5) (4,10)
+(6,8) (6,10)
+(8,10)
+
+total pair = 10
+ */
+
+public class pairinarray {
+    public static void printPairs(int numbers[]){
+        int tp = 0;
+        for(int i = 0; i<numbers.length; i++){
+            int curr = numbers[i];
+            for(int j= i+1; j<numbers.length; j++){
+                System.out.print("("+ curr + ","+ numbers[j]+")");
+            }
+        }
+        System.out.println("total pairs = " + tp);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        printPairs(numbers);
+        
+    }
+    
+}
